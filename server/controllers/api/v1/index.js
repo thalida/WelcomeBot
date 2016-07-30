c9f86074d817bb591fb2f76c73c100a555e49305
Thalida Noel <thalida.c.noel@gmail.com>
2016-07-30 16:07:51 -0400

    Added post url

--- server/controllers/api/v1/index.js
+++ server/controllers/api/v1/index.js
@@ -11,4 +11,8 @@ api.get('/slack/welcome', function(req, res) {
   res.send('Hello from APIv1 root route.');
 });
 
+api.post('/slack/welcome/post', function(req, res) {
+  res.send('post from Hello from APIv1 root route.');
+});
+
 module.exports = api;
