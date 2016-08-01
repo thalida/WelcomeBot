fc9f1c2abacc58568832f273a496272bfd1567fc
Thalida Noel <thalida.c.noel@gmail.com>
2016-08-01 00:48:36 -0400

    Reorganized response messages

--- server/slackbot/commands/welcome.js
+++ server/slackbot/commands/welcome.js
@@ -1,11 +1,107 @@
-var ALLOWED_ARGS = ['', 'help', 'edit', 'remove', 'set', 'set:file', 'set:text'];
-
 var welcomeCommand = {
+    ALLOWED_ARGS: ['', 'help', 'edit', 'remove', 'set', 'set:file', 'set:text'],
+    messages: {
+        getErrorMessage: function(){
+            return {
+                attachments: [{
+                    title: "Oh no! WelcomeBot encountered an error!",
+                    text: [
+                        'Please try again',
+                        '_Need help? Try typing: `/welcome help`_'
+                    ].join('\n'),
+                    color: 'bad',
+                    mrkdwn_in: ["pretext", "text", "fields"]
+                }]
+            };
+        },
+        getArgsError: function( args ){
+            var errorMessage = this.getErrorMessage();
+            errorMessage.attachments[0].text = [
+                "Oh dear, it seems I don't know how to `" + args.key + "`!",
+                '_Need help? Try typing: `/welcome help`_'
+            ].join('\n');
+            return errorMessage;
+        },
+        getNoMessageError: function(){
+            var errorMessage = this.getErrorMessage();
+            errorMessage.attachments[0].title = "Oh no! There's no welcome message!";
+            errorMessage.attachments[0].text = [
+                'Create one using `/welcome set`.',
+                '_Need help? Try typing: `/welcome help`_'
+            ].join('\n');
+
+            return errorMessage;
+        },
+        getNoFileError: function(){
+            var errorMessage = this.getErrorMessage();
+            errorMessage.attachments[0].title = "Oh no! There's no welcome message file!";
+            errorMessage.attachments[0].text = [
+                'Create one using `/welcome set`.',
+                '_Need help? Try typing: `/welcome help`_'
+            ].join('\n');
+
+            return errorMessage;
+        },
+        getMessageRemoveError: function(){
+            var errorMessage = this.getErrorMessage();
+            errorMessage.attachments[0].text = [
+                "There was an error removing the message.",
+                'Please try again',
+                '_Need help? Try typing: `/welcome help`_'
+            ].join('\n');
+
+            return errorMessage;
+        },
+        getMessageSuccess: function( res ){
+            return {
+                attachments: [{
+                    title: res.file.title,
+                    title_link: res.file.permalink,
+                    text: res.content || res.content_html,
+                    color: '#000000',
+                    unfurl_links: false,
+                    unfurl_media: false,
+                    mrkdwn_in: ["pretext", "text", "fields"]
+                }]
+            };
+        },
+        getCreatePrivateSuccess: function(){
+            return "Awesome! I'll let to the team know about the updates!";
+        },
+        getCreatePublicSuccess: function( res, message ){
+            return {
+                attachments: [
+                    {
+                        title: 'New Welcome Message',
+                        pretext: '<@' + message.user + '> updated the welcome message!',
+                        text: '_Type `/welcome` to see the full message._\n\n',
+                        fields: [
+                            {
+                                title: 'Snippet',
+                                value: res.file.preview.trim() + '...',
+                                short: true
+                            }
+                        ],
+                        unfurl_links: false,
+                        unfurl_media: false,
+                        mrkdwn_in: ["pretext", "text", "fields"],
+                        color: 'good'
+                    }
+                ]
+            };
+        },
+        getMessageEditSuccess: function( res ){
+            return 'Edit the welcome file at:' + res.file.permalink
+        },
+        getMessageRemoveSuccess: function( res ){
+            return "You've removed the welcome message!"
+        }
+    },
     run: function( controller, bot, message ){
         var args = this.processArgs( message );
 
-        if( ALLOWED_ARGS.indexOf[args.key] === -1 ){
-            bot.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
+        if(this.ALLOWED_ARGS.indexOf(args.key) === -1 ){
+            bot.replyPublic(message, this.messages.getArgsError( args ));
             return;
         }
 
@@ -33,7 +129,7 @@ var welcomeCommand = {
                 break;
         };
 
-        argFunc( args, controller, bot, message );
+        argFunc.call( this, args, controller, bot, message );
     },
     processArgs: function( message ){
         var command = message.text.trim().split(/\s/, 1)[0];
@@ -45,21 +141,11 @@ var welcomeCommand = {
         bot.replyPrivate(message, {
             attachments: [
                 {
-                    pretext: '*/welcome help*',
+                    pretext: '*WelcomeBot Help*',
                     fields: [
                         {
                             "title": "/welcome",
                             "value": "Displays the welcome message",
-                            "short": false
-                        },
-                        {
-                            "title": "/welcome edit",
-                            "value": "Returns a link to edit the welcome message",
-                            "short": false
-                        },
-                        {
-                            "title": "/welcome remove",
-                            "value": "Removes the welcome message",
                             "short": false
                         },
                         {
@@ -71,6 +157,16 @@ var welcomeCommand = {
                             "title": "/welcome set:file [slack file link]",
                             "value": "Sets welcome message to the contents of the file.",
                             "short": false
+                        },
+                        {
+                            "title": "/welcome edit",
+                            "value": "Returns a link to edit the welcome message",
+                            "short": false
+                        },
+                        {
+                            "title": "/welcome remove",
+                            "value": "Removes the welcome message",
+                            "short": false
                         }
                     ],
                     mrkdwn_in: ["pretext", "text", "fields"]
@@ -78,59 +174,28 @@ var welcomeCommand = {
             ]
         });
     },
-    edit: function( args, controller, bot, message ){
+    outputText: function( args, controller, bot, message ){
+        var self = this;
+
         controller.storage.channels.get(message.channel, function(err, channel) {
             if (!channel || !channel.welcomeFileId ) {
-                bot.replyPublic(message, 'Oh no! No welcome message has been setup. \n Create using `/welcome set`');
+                bot.replyPublic(message, self.messages.getNoMessageError());
                 return;
             }
 
             bot.api.files.info({file: channel.welcomeFileId}, function(err, res){
                 if( err ){
-                    bot.replyPublic(message, "Oh no! That file doesn't exist anymore!");
-                } else {
-                    bot.replyPrivate(message, 'Edit the welcome file at:' + res.file.permalink);
+                    bot.replyPublic(message, self.messages.getNoFileError());
+                    return;
                 }
+
+                bot.replyPrivate(message, self.messages.getMessageSuccess( res ));
             });
         });
     },
-    remove: function( args, controller, bot, message ){
-        controller.storage.channels.get(message.channel, function(err, channel) {
-            if( !channel ){
-                bot.replyPublic(message, 'Oh no! No welcome message has been setup.');
-                return;
-            }
-
-            var fileId = channel.welcomeFileId;
-
-            if( args.value ){
-                var url = args.value;
-                var urlParts = url.split('/');
-                fileId = urlParts[ urlParts.length - 2 ];
-            }
-
-            if( !fileId ){
-                bot.replyPublic(message, 'Oh no! No welcome message has been setup.');
-                return;
-            }
-
-            if( fileId ){
-                bot.api.files.delete({file: fileId}, function(){
-                    if( err ){
-                        bot.replyPublic(message, "Oh no! There was an error removing the WelcomeBot snippet!");
-                        return;
-                    }
-
-                    channel.welcomeFileId = null;
-
-                    bot.replyPublic(message, 'welcome message removed!');
-
-                    controller.storage.channels.save(channel);
-                });
-            }
-        });
-    },
     setText: function( args, controller, bot, message ){
+        var self = this;
+
         var time = new Date().getTime();
         var params = {
             filename: 'welcomebot_' + time,
@@ -153,45 +218,28 @@ var welcomeCommand = {
             bot.api.files.upload(params, function(err, res){
                 if( err ){
                     channel.welcomeFileId = null;
-                    bot.replyPublic(message, "Oh no! There was an error creating a WelcomeBot snippet!");
+                    bot.replyPublic(message, self.messages.getErrorMessage());
                     return;
                 }
 
                 channel.welcomeFileId = res.file.id;
-                bot.replyPrivate(message, "Awesome! I'll let to the team know about the updates!");
-                bot.reply(message, {
-                    attachments: [
-                        {
-                            title: 'New Welcome Message',
-                            pretext: '<@' + message.user + '> updated the welcome message!',
-                            text: '_Type `/welcome` to see the full message._\n\n',
-                            fields: [
-                                {
-                                    title: 'Snippet',
-                                    value: res.file.preview.trim() + '...',
-                                    short: true
-                                }
-                            ],
-                            unfurl_links: false,
-                            unfurl_media: false,
-                            mrkdwn_in: ["pretext", "text", "fields"],
-                            color: 'good'
-                        }
-                    ]
-                });
+
+                bot.replyPrivate(message, self.messages.getCreatePrivateSuccess());
+                bot.reply(message, self.messages.getCreatePublicSuccess( res, message ));
 
                 controller.storage.channels.save(channel);
             });
         });
     },
     setFile: function( args, controller, bot, message ){
+        var self = this;
         var url = args.value;
         var urlParts = url.split('/');
         var fileId = urlParts[ urlParts.length - 2 ];
 
         bot.api.files.info({file: fileId}, function(err, res){
             if( err ){
-                bot.replyPublic(message, "Oh no! That file doesn't exist!");
+                 bot.replyPublic(message, self.messages.getNoFileError());
                 return;
             }
 
@@ -205,56 +253,67 @@ var welcomeCommand = {
 
                 channel.welcomeFileId = fileId;
 
-                bot.replyPublic(message, {
-                    attachments: [
-                        {
-                            title: 'New Welcome Message',
-                            title_link: res.file.permalink,
-                            fields: [
-                                {
-                                    title: 'Title',
-                                    value: res.file.title
-                                }
-                            ],
-                            unfurl_links: false,
-                            unfurl_media: false,
-                            mrkdwn_in: ["pretext", "text", "fields"],
-                            color: 'good'
-                        }
-                    ]
-                });
+                bot.replyPrivate(message, self.messages.getCreatePrivateSuccess());
+                bot.reply(message, self.messages.getCreatePublicSuccess( res, message ));
 
                 controller.storage.channels.save(channel);
             });
         });
     },
-    outputText: function( args, controller, bot, message ){
+    edit: function( args, controller, bot, message ){
+        var self = this;
+
         controller.storage.channels.get(message.channel, function(err, channel) {
             if (!channel || !channel.welcomeFileId ) {
-                bot.replyPublic(message, 'Oh no! No welcome message has been setup.');
+                bot.replyPublic(message, self.messages.getNoMessageError());
                 return;
             }
 
             bot.api.files.info({file: channel.welcomeFileId}, function(err, res){
                 if( err ){
-                    bot.replyPublic(message, "Oh no! That file doesn't exist anymore!");
-                    return;
+                    bot.replyPublic(message, self.messages.getNoFileError());
+                } else {
+                    bot.replyPrivate(message, self.messages.getMessageEditSuccess( res ));
                 }
-
-                bot.replyPrivate(message, {
-                    attachments: [
-                        {
-                            title: res.file.title,
-                            title_link: res.file.permalink,
-                            text: res.content,
-                            color: '#000000',
-                            unfurl_links: false,
-                            unfurl_media: false,
-                            mrkdwn_in: ["pretext", "text", "fields"]
-                        }
-                    ]
-                });
             });
+        });
+    },
+    remove: function( args, controller, bot, message ){
+        var self = this;
+
+        controller.storage.channels.get(message.channel, function(err, channel) {
+            if( !channel ){
+                bot.replyPublic(message, self.messages.getNoMessageError());
+                return;
+            }
+
+            var fileId = channel.welcomeFileId;
+
+            if( args.value ){
+                var url = args.value;
+                var urlParts = url.split('/');
+                fileId = urlParts[ urlParts.length - 2 ];
+            }
+
+            if( !fileId ){
+                bot.replyPublic(message, self.messages.getNoMessageError());
+                return;
+            }
+
+            if( fileId ){
+                bot.api.files.delete({file: fileId}, function(){
+                    if( err ){
+                        bot.replyPublic(message, self.messages.getMessageRemoveError());
+                        return;
+                    }
+
+                    channel.welcomeFileId = null;
+
+                    bot.replyPublic(message, self.messages.getMessageRemoveSuccess());
+
+                    controller.storage.channels.save(channel);
+                });
+            }
         });
     }
 };
